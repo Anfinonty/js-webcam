29f923718321b17e3245916df90241bb96e6ed41
Anfinonty <97777854+Anfinonty@users.noreply.github.com>
2023-12-04 12:11:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+video = document.getElementById("myvideo");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => console.error(err));
+}
+
+
+getVideo();
